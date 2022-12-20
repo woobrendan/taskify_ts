7975262a7b45d97babe37f1a0309274be2a6b406
Brendan Woo <woo.brendan@gmail.com>
2022-12-20 12:16:58 -0800

    move type information into its own file, out of app

---
+++ types.ts
@@ -0,0 +1,20 @@
+let name:string = 'Brendan'
+let age: number | string;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+
+// age = 
+
+// type Person = {
+//   name: string,
+//   age?: number
+// }
+// let person: Person = {
+//   name: 'George',
+//   age: 22 
+// };
+
+// let lotsOfPeople: Person[];
+
+export{}
