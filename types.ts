2969d08ad500a573f4d8f014a261b015ed9a175b
Brendan Woo <woo.brendan@gmail.com>
2022-12-21 13:44:14 -0800

    add types and comments. add method types and rturn types

--- types.ts
+++ types.ts
@@ -1,20 +1,32 @@
 let name:string = 'Brendan'
+
+// use union | to let a variable have multiple types
 let age: number | string;
 let isStudent: boolean;
 let hobbies: string[];
 let role: [number, string];
 
-// age = 
+//add question mark after key means that the it does not have to be initialized. optional
+type Person = {
+  name: string,
+  age?: number
+}
+let george: Person = {
+  name: 'George',
+  age: 22 
+};
 
-// type Person = {
-//   name: string,
-//   age?: number
+//Create an array of type Person
+let lotsOfPeople: Person[];
+
+//unknown better than type any
+let personName: unknown;
+
+// void returns undefined, never doesnt return anything
+let printName: (name: string) => never;
+
+// const printName = (name: string) => {
+//   console.log('name:', name)
 // }
-// let person: Person = {
-//   name: 'George',
-//   age: 22 
-// };
-
-// let lotsOfPeople: Person[];
 
 export{}
