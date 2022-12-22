5903e3a5d3e3052045d2eea22fcb1e6e5aa71fe4
Brendan Woo <woo.brendan@gmail.com>
2022-12-22 11:41:35 -0800

    add state toDoList, importing interface ToDo and applying to that state. add method handleAdd and pass to inputField

--- taskify/src/App.tsx
+++ taskify/src/App.tsx
@@ -1,16 +1,22 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './Styles/App.scss';
 import InputField from './Components/InputField';
+import { ToDo } from './model';
 
 //React functional component
 const App: React.FC = () => {
 
   const [toDo, setToDo] = useState<string>('')
+  const [toDoList, setToDoList] = useState<ToDo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault()
+  }
 
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField toDo={toDo} setToDo={setToDo}/>
+      <InputField toDo={toDo} setToDo={setToDo} handleAdd={handleAdd}/>
     </div>
   );
 }
