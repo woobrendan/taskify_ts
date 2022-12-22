d196184e4967ac476309f8772a66bb5bfe1d1b1e
Brendan Woo <woo.brendan@gmail.com>
2022-12-22 11:18:10 -0800

    amport input field component and render under span

--- taskify/src/App.tsx
+++ taskify/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import './App.scss';
+import './Styles/App.scss';
+import InputField from './Components/InputField';
 
 //React functional component
 const App: React.FC = () => {
   return (
     <div className="App">
       <span className="heading">Taskify</span>
+      <InputField />
     </div>
   );
 }
