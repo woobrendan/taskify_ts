50d57af8a06e30ead102a066195ff6462efad17f
Brendan Woo <woo.brendan@gmail.com>
2022-12-22 11:41:58 -0800

    add handleAdd to props giving type formevent. passing method to form as onSubmit

--- taskify/src/Components/InputField.tsx
+++ taskify/src/Components/InputField.tsx
@@ -4,11 +4,12 @@ import '../Styles/styles.scss';
 interface Props {
   toDo: string;
   setToDo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField: React.FC<Props> = ({ toDo, setToDo }) => {
+const InputField: React.FC<Props> = ({ toDo, setToDo, handleAdd }) => {
   return (
-    <form className='input'>
+    <form className='input' onSubmit={handleAdd}>
       <input 
         type='input' 
         placeholder="Enter a task" 
