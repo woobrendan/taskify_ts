e5b222e609e8404b0a89212377d54daad0019e1a
Brendan Woo <woo.brendan@gmail.com>
2022-12-22 11:17:54 -0800

    import style sheet. change div to form, add input element and button

--- taskify/src/Components/InputField.tsx
+++ taskify/src/Components/InputField.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import '../Styles/styles.scss'
 
-function InputField() {
+const InputField = () => {
   return (
-    <div>
-      
-    </div>
+    <form className='input'>
+      <input type='input' placeholder="Enter a task" className='input__box' />
+      <button className="input__submit" type="submit">Go</button>
+    </form>
   )
 }
 
