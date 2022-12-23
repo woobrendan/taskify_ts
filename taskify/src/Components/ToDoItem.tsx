5317f032bdc970b3341f3212272056c541c8e7ec
Brendan Woo <woo.brendan@gmail.com>
2022-12-23 13:57:23 -0800

    import style sheet. create method handleDone to loop through and find toDo id and set is done to false. add method to onclick for done icon

--- taskify/src/Components/ToDoItem.tsx
+++ taskify/src/Components/ToDoItem.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ToDo } from "../model";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
+import "../Styles/styles.scss";
 
 type Props = {
   toDo: ToDo;
@@ -10,6 +11,14 @@ type Props = {
 };
 
 const ToDoItem: React.FC<Props> = ({ toDo, toDos, setToDos }) => {
+  const handleDone = (id: number) => {
+    setToDos(
+      toDos.map((task) =>
+        task.id === id ? { ...task, isDone: !task.isDone } : task,
+      ),
+    );
+  };
+
   return (
     <form className="toDos__single">
       <span className="toDos__single--text">{toDo.toDo}</span>
@@ -20,7 +29,7 @@ const ToDoItem: React.FC<Props> = ({ toDo, toDos, setToDos }) => {
         <span className="icon">
           <AiFillDelete />
         </span>
-        <span className="icon">
+        <span className="icon" onClick={() => handleDone(toDo.id)}>
           <MdDone />
         </span>
       </div>
