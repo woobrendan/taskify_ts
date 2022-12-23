5c3a94bf32400efdc941184ecb12985d1f6ec852
Brendan Woo <woo.brendan@gmail.com>
2022-12-23 12:31:34 -0800

    import 3 icons from react icons. render a div and 3 spans with icons

--- taskify/src/Components/ToDoItem.tsx
+++ taskify/src/Components/ToDoItem.tsx
@@ -1,19 +1,31 @@
-import React from 'react'
-import { ToDo } from '../model'
+import React from "react";
+import { ToDo } from "../model";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
 
 type Props = {
   toDo: ToDo;
   toDos: ToDo[];
-  setToDos: React.Dispatch<React.SetStateAction<ToDo[]>> 
-}
+  setToDos: React.Dispatch<React.SetStateAction<ToDo[]>>;
+};
 
 const ToDoItem: React.FC<Props> = ({ toDo, toDos, setToDos }) => {
   return (
     <form className="toDos__single">
-      <span className="toDos__single--text"></span>
+      <span className="toDos__single--text">{toDo.toDo}</span>
+      <div>
+        <span className="icon">
+          <AiFillEdit />
+        </span>
+        <span className="icon">
+          <AiFillDelete />
+        </span>
+        <span className="icon">
+          <MdDone />
+        </span>
+      </div>
     </form>
-  )
-}
+  );
+};
 
-export default ToDoItem
-
+export default ToDoItem;
