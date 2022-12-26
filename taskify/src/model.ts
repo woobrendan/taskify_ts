020fb852e2529d877fdd0f08a8908552b3d4219c
Brendan Woo <woo.brendan@gmail.com>
2022-12-26 14:50:51 -0500

    create template of use reducers with actions type, method todoreducer and a reducer example

--- taskify/src/model.ts
+++ taskify/src/model.ts
@@ -3,3 +3,16 @@ export interface ToDo {
   toDo: string;
   isDone: boolean;
 }
+
+type Actions =
+  | { type: "add"; payload: string }
+  | { type: "remove"; payload: number }
+  | { type: "done"; payload: number };
+
+const ToDoReducer = (state: ToDo[], action: Actions) => {};
+
+import { useReducer } from "react";
+
+const reducerExample = () => {
+  const [state, dispatch] = useReducer(ToDoReducer, []);
+};
