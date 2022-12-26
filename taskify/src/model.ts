41684544131267fdbb4bc5767ca9716a26e9b8e4
Brendan Woo <woo.brendan@gmail.com>
2022-12-26 14:58:43 -0500

    complete logic for switch case actions add, remove and done

--- taskify/src/model.ts
+++ taskify/src/model.ts
@@ -9,7 +9,23 @@ type Actions =
   | { type: "remove"; payload: number }
   | { type: "done"; payload: number };
 
-const ToDoReducer = (state: ToDo[], action: Actions) => {};
+const ToDoReducer = (state: ToDo[], action: Actions) => {
+  switch (action.type) {
+    case "add":
+      return [
+        ...state,
+        { id: Date.now(), toDo: action.payload, isDone: false },
+      ];
+    case "remove":
+      return state.filter((toDo) => toDo.id !== action.payload);
+    case "done":
+      return state.map((toDo) =>
+        toDo.id !== action.payload ? { ...toDo, isDone: !toDo.isDone } : toDo,
+      );
+    default:
+      return state;
+  }
+};
 
 import { useReducer } from "react";
 
