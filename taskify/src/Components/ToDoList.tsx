1abe15284f0433ba31c4b5709e0267d4f97703ec
Brendan Woo <woo.brendan@gmail.com>
2022-12-27 14:01:12 -0500

    comment out all references to setToDos in prep for usage of redux for setting to dos

--- taskify/src/Components/ToDoList.tsx
+++ taskify/src/Components/ToDoList.tsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import { ToDo } from '../model'
-import '../Styles/styles.scss'
-import ToDoItem from './ToDoItem'
+import React from "react";
+import { ToDo } from "../model";
+import "../Styles/styles.scss";
+import ToDoItem from "./ToDoItem";
 
 interface Props {
   toDos: ToDo[];
-  setToDos: React.Dispatch<React.SetStateAction<ToDo[]>>
+  // setToDos: React.Dispatch<React.SetStateAction<ToDo[]>>
 }
 
-const ToDoList: React.FC<Props> = ({ toDos, setToDos }) => {
+const ToDoList: React.FC<Props> = ({ toDos /*setToDos*/ }) => {
   return (
-    <div className='toDos'>
-      {toDos.map(toDo => (
-        <ToDoItem 
-          key={toDo.id} 
-          toDo={toDo} 
+    <div className="toDos">
+      {toDos.map((toDo) => (
+        <ToDoItem
+          key={toDo.id}
+          toDo={toDo}
           toDos={toDos}
-          setToDos={setToDos}
-          />
+          // setToDos={setToDos}
+        />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default ToDoList
+export default ToDoList;
