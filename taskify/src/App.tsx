48f43f40927ea5efd9810a6e49e30342d0ae6701
Brendan Woo <woo.brendan@gmail.com>
2022-12-27 14:32:56 -0500

    remove props for todos passed to ToDolist. remove useAppSelector import and remove variable toDos imported from that

--- taskify/src/App.tsx
+++ taskify/src/App.tsx
@@ -2,15 +2,13 @@ import React, { useState } from "react";
 import "./Styles/App.scss";
 import InputField from "./Components/InputField";
 import ToDoList from "./Components/ToDoList";
-import { useAppDispatch, useAppSelector } from "./Store/hooks";
+import { useAppDispatch } from "./Store/hooks";
 import { toDoActions } from "./Store/toDoSlice";
 
 //React functional component
 const App: React.FC = () => {
   const [toDo, setToDo] = useState<string>("");
   const dispatch = useAppDispatch();
-
-  const toDos = useAppSelector((state) => state.toDo.toDos);
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
@@ -24,7 +22,7 @@ const App: React.FC = () => {
     <div className="App">
       <span className="heading">Taskify</span>
       <InputField toDo={toDo} setToDo={setToDo} handleAdd={handleAdd} />
-      <ToDoList toDos={toDos} />
+      <ToDoList />
     </div>
   );
 };
