ff2efb053734c58ab28dce45443376725e1b4b03
Brendan Woo <woo.brendan@gmail.com>
2022-12-27 13:35:25 -0500

    comment out reducer information, keep just interface

--- taskify/src/model.ts
+++ taskify/src/model.ts
@@ -4,31 +4,31 @@ export interface ToDo {
   isDone: boolean;
 }
 
-type Actions =
-  | { type: "add"; payload: string }
-  | { type: "remove"; payload: number }
-  | { type: "done"; payload: number };
+// type Actions =
+//   | { type: "add"; payload: string }
+//   | { type: "remove"; payload: number }
+//   | { type: "done"; payload: number };
 
-const ToDoReducer = (state: ToDo[], action: Actions) => {
-  switch (action.type) {
-    case "add":
-      return [
-        ...state,
-        { id: Date.now(), toDo: action.payload, isDone: false },
-      ];
-    case "remove":
-      return state.filter((toDo) => toDo.id !== action.payload);
-    case "done":
-      return state.map((toDo) =>
-        toDo.id !== action.payload ? { ...toDo, isDone: !toDo.isDone } : toDo,
-      );
-    default:
-      return state;
-  }
-};
+// const ToDoReducer = (state: ToDo[], action: Actions) => {
+//   switch (action.type) {
+//     case "add":
+//       return [
+//         ...state,
+//         { id: Date.now(), toDo: action.payload, isDone: false },
+//       ];
+//     case "remove":
+//       return state.filter((toDo) => toDo.id !== action.payload);
+//     case "done":
+//       return state.map((toDo) =>
+//         toDo.id !== action.payload ? { ...toDo, isDone: !toDo.isDone } : toDo,
+//       );
+//     default:
+//       return state;
+//   }
+// };
 
-import { useReducer } from "react";
+// import { useReducer } from "react";
 
-const reducerExample = () => {
-  const [state, dispatch] = useReducer(ToDoReducer, []);
-};
+// const reducerExample = () => {
+//   const [state, dispatch] = useReducer(ToDoReducer, []);
+// };
