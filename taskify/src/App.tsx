7ea35e87d9f84db00b22cc4184e1b947ddc462d9
Brendan Woo <woo.brendan@gmail.com>
2022-12-27 13:48:44 -0500

    replace usestate usage for toDos handling over to toDos from redux store for toDo. replcae setToDos with dispatch toDoActions

--- taskify/src/App.tsx
+++ taskify/src/App.tsx
@@ -1,31 +1,34 @@
-import React, { useState } from 'react';
-import './Styles/App.scss';
-import InputField from './Components/InputField';
-import { ToDo } from './model';
-import ToDoList from './Components/ToDoList';
+import React, { useState } from "react";
+import "./Styles/App.scss";
+import InputField from "./Components/InputField";
+import { ToDo } from "./model";
+import ToDoList from "./Components/ToDoList";
+import { useAppDispatch, useAppSelector } from "./Store/hooks";
+import { toDoActions } from "./Store/toDoSlice";
 
 //React functional component
 const App: React.FC = () => {
+  const [toDo, setToDo] = useState<string>("");
+  const dispatch = useAppDispatch();
+  // const [toDos, setToDos] = useState<ToDo[]>([]);
 
-  const [toDo, setToDo] = useState<string>('')
-  const [toDos, setToDos] = useState<ToDo[]>([]);
+  const toDos = useAppSelector((state) => state.toDo.toDos);
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
-
     if (toDo) {
-      setToDos([...toDos, {id: Date.now(), toDo, isDone: false}])
-      setToDo('')
+      dispatch(toDoActions.addToDo(toDo));
+      setToDo("");
     }
-  }
+  };
 
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField toDo={toDo} setToDo={setToDo} handleAdd={handleAdd}/>
-      <ToDoList toDos={toDos} setToDos={setToDos}/>
+      <InputField toDo={toDo} setToDo={setToDo} handleAdd={handleAdd} />
+      <ToDoList toDos={toDos} /*setToDos={setToDos}*/ />
     </div>
   );
-}
+};
 
 export default App;
