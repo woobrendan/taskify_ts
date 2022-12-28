6c112aa81f1da27c1fbf7d4c912a6fb41b192414
Brendan Woo <woo.brendan@gmail.com>
2022-12-28 15:29:41 -0500

    wrap toDos divs in seperate Droppable components. give droppableId to both. provide function to each droppable returning the div toDos and toDos remove. for each, give a ref and spread provided.droppableProps

--- taskify/src/Components/ToDoList.tsx
+++ taskify/src/Components/ToDoList.tsx
@@ -3,34 +3,46 @@ import "../Styles/styles.scss";
 import ToDoItem from "./ToDoItem";
 import { useAppSelector } from "../Store/hooks";
 import { ToDo } from "../model";
+import { Droppable } from "react-beautiful-dnd";
 
 interface Props {
   completedToDos: ToDo[];
   setCompletedToDos: React.Dispatch<React.SetStateAction<ToDo[]>>;
 }
 
-const ToDoList: React.FC<Props> = (completedToDos, setCompletedToDos) => {
+const ToDoList: React.FC<Props> = ({ completedToDos, setCompletedToDos }) => {
   const toDos = useAppSelector((state) => state.toDo.toDos);
 
   return (
-    // <div className="toDos">
-    //   {toDos.map((toDo) => (
-    //     <ToDoItem key={toDo.id} toDo={toDo} />
-    //   ))}
-    // </div>
     <div className="container">
-      <div className="toDos">
-        <span className="toDos__heading">Active Tasks</span>
-        {toDos.map((toDo) => (
-          <ToDoItem key={toDo.id} toDo={toDo} />
-        ))}
-      </div>
-      <div className="toDos remove">
-        <span className="toDos__heading">Completed Tasks</span>
-        {toDos.map((toDo) => (
-          <ToDoItem key={toDo.id} toDo={toDo} />
-        ))}
-      </div>
+      <Droppable droppableId="ToDosList">
+        {(provided) => (
+          <div
+            className="toDos"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <span className="toDos__heading">Active Tasks</span>
+            {toDos.map((toDo) => (
+              <ToDoItem key={toDo.id} toDo={toDo} />
+            ))}
+          </div>
+        )}
+      </Droppable>
+      <Droppable droppableId="ToDosRemove">
+        {(provided) => (
+          <div
+            className="toDos remove"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <span className="toDos__heading">Completed Tasks</span>
+            {toDos.map((toDo) => (
+              <ToDoItem key={toDo.id} toDo={toDo} />
+            ))}
+          </div>
+        )}
+      </Droppable>
     </div>
   );
 };
