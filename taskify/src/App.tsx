06d634edaeb2bd528f689ba4365f83fe1f6fbf66
Brendan Woo <woo.brendan@gmail.com>
2022-12-28 16:04:02 -0500

    pass props onDragEnd to dragDropContext. import useAppSelector to get toDos

--- taskify/src/App.tsx
+++ taskify/src/App.tsx
@@ -22,7 +22,7 @@ const App: React.FC = () => {
   };
 
   return (
-    <DragDropContext>
+    <DragDropContext onDragEnd={() => {}}>
       <div className="App">
         <span className="heading">Taskify</span>
         <InputField toDo={toDo} setToDo={setToDo} handleAdd={handleAdd} />
