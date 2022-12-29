0b2c7510763049c7a0382bea3bf3f8e250d0787a
Brendan Woo <woo.brendan@gmail.com>
2022-12-29 13:33:37 -0500

    add method onDragEnd. create logic to exit and do nothing if no destination or id/index of srouce and destination are the same

--- taskify/src/App.tsx
+++ taskify/src/App.tsx
@@ -4,18 +4,39 @@ import InputField from "./Components/InputField";
 import ToDoList from "./Components/ToDoList";
 import { useAppDispatch } from "./Store/hooks";
 import { toDoActions } from "./Store/toDoSlice";
-import { DragDropContext } from "react-beautiful-dnd";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
+import { useAppSelector } from "./Store/hooks";
 
 //React functional component
 const App: React.FC = () => {
   const [toDo, setToDo] = useState<string>("");
   const dispatch = useAppDispatch();
 
+  const toDos = useAppSelector((state) => state.toDo.toDos);
+  const completedToDos = useAppSelector((state) => state.toDo.completedToDos);
+
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
     if (toDo) {
       dispatch(toDoActions.addToDo(toDo));
       setToDo("");
+    }
+  };
+
+  const onDragEnd = (result: DropResult) => {
+    const { source, destination } = result;
+    if (!destination) return;
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return;
+
+    let add,
+      active = toDos,
+      complete = completedToDos;
+    if (source.droppableId === "ToDosList") {
+      add = active[source.index];
     }
   };
 
